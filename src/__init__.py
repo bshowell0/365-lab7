a120a2c11e15c9d351ae9e2915a868ce2de985f3
Brandon Howell <bshowell@calpoly.edu>
2024-03-20 01:05:08 -0700

    manual set of src files to make python undefined checker chill out

--- src/__init__.py
+++ src/__init__.py
@@ -4,4 +4,5 @@ import os
 files = os.listdir(os.path.dirname(os.path.realpath(__file__)))
 files = [file[:-3] for file in files if file.endswith(".py") and "_script" not in file and file != "__init__.py"]
 
-__all__ = files
+# __all__ = files
+__all__ = ['printer', 'request', 'validate']
