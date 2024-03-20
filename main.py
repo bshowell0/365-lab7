c7c974901b0acbc578137066d485500cae0009b6
Brandon Howell <bshowell@calpoly.edu>
2024-03-19 20:08:56 -0700

    basic structure of main

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ from dotenv import load_dotenv
 from os import getenv
 import mysql.connector
 
-load_dotenv()  # take environment variables from .env.
+load_dotenv()
 
 
-# Code of your application, which uses environment variables (e.g. from `os.environ` or
-# `os.getenv`) as if they came from the actual environment.
+def main():
+    conn = mysql.connector.connect(user=getenv('DB_USER'),
+                                   password=getenv('DB_PASSWORD'),
+                                   host='mysql.labthreesixfive.com',
+                                   database=getenv('DB_NAME'))
 
-conn = mysql.connector.connect(user=getenv('DB_USER'), password=getenv('DB_PASSWORD'),
-                               host='mysql.labthreesixfive.com',
-                               database=getenv('DB_NAME'))
-
-print(conn)
-
-cursor = conn.cursor()
-cursor.execute("SELECT * FROM hp_goods WHERE flavor = 'Chocolate'")
-result = cursor.fetchall()
-print(result)
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM hp_goods WHERE flavor = 'Chocolate'")
+    result = cursor.fetchall()
+    print(result)
 
 
+
+
+if __name__ == "__main__":
+    main()
